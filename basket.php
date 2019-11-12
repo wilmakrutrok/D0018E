d547e875e7ebb6899423d62d18e28d6e08f1b9d8
Mark5213 <marcuse0@hotmail.com>
2019-11-12 10:57:45 +0100

    Update basket.php

--- basket.php
+++ basket.php
@@ -2,15 +2,23 @@
 include "config.php";
 
 include "products.php";
+$price = 0;
+$totalprice = 0;
 
 
-function add() {
+function add(item,price) {
+    $price = price;
     echo "added to basket";
+    $totalprice == $price + $totalprice;
 }
 
 
-function remove() {
+function remove(item,price) {
+    $price = price;
+    
     echo "removed from basket";
+    $totalprice == $totalprice - $price;
+    
 }
 
 
