696df38bcd75d17bc31e15cd9e0dee7f16105f83
André Christofferson <36477978+andregossip@users.noreply.github.com>
2019-11-21 17:08:45 +0100

    Update login.php

--- login.php
+++ login.php
@@ -5,14 +5,14 @@ if(isset($_POST['submit'])){
     $uname = mysqli_real_escape_string($conn,$_POST['username']);
     $password = mysqli_real_escape_string($conn,$_POST['password']);
     
+    //check if username and password is entered and searches
+    //for a hit on the username.If there is a hit the password
+    //is checked against the hashed password in the database.
     if ($uname != "" && $password != ""){
-        
-       // $sql_query = "select count(*) as cntUser from users where username='".$uname."' and password='".$password."'";
         $sql_query = "select count(*) as cntUser from users where username='".$uname."'";
         $result = mysqli_query($conn,$sql_query);
         $row = mysqli_fetch_array($result);
         $count = $row['cntUser'];
-        
         if($count > 0){
             $sql_query = "select password from users where username='".$uname."'";
             $result = mysqli_query($conn,$sql_query);
