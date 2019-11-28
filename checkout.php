d8cec81fa4c4052b6f4b18e4ccb257b204fcec08
wilmakrutrok <47818758+wilmakrutrok@users.noreply.github.com>
2019-11-28 11:08:32 +0100

    Add files via upload

--- checkout.php
+++ checkout.php
@@ -8,7 +8,6 @@
   $query_getuid= "select iduser from users where username='".$uname."'";
   $result_uid = mysqli_query($conn, $query_getuid);
   $uid = mysqli_fetch_array($result_uid);
-
   //Här hämtar jag ut alla produkter som finns i kundkorgen. 
   //Priset borde hämtas från cartproductstabellen men den är 
   //null där så hämtar från products direkt för nu
@@ -20,9 +19,6 @@
     ON cartproducts.idproduct = products.idproduct
   WHERE carttouser.iduser = '".$uid['iduser']."'";
   $result_cart = $conn->query($query_cart);
-
-
-
   if(isset($_POST['pay_button'])){  
  /*
   $query_create_order = "INSERT INTO orders(iduser)
@@ -48,124 +44,6 @@
 <!--<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 -->
-
-<style>
-body {
-  font-family: Arial;
-  font-size: 17px;
-  padding: 8px;
-}
-* {
-  box-sizing: border-box;
-}
-.row {
-  display: -ms-flexbox; /* IE10 */
-  display: flex;
-  -ms-flex-wrap: wrap; /* IE10 */
-  flex-wrap: wrap;
-  margin: 0 -16px;
-}
-.col-25 {
-  -ms-flex: 25%; /* IE10 */
-  flex: 25%;
-}
-.col-50 {
-  -ms-flex: 50%; /* IE10 */
-  flex: 50%;
-}
-.col-75 {
-  -ms-flex: 75%; /* IE10 */
-  flex: 75%;
-}
-.col-25,
-.col-50,
-.col-75 {
-  padding: 0 0px;
-}
-.container {
-  background-color: #f2f2f2;
-  padding: 5px 5px 5px 5px;
-  border: 1px solid lightgrey;
-  border-radius: 3px;
-}
-input[type=text] {
-  width: 100%;
-  margin-bottom: 20px;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-}
-label {
-  margin-bottom: 10px;
-  display: block;
-}
-.icon-container {
-  margin-bottom: 20px;
-  padding: 7px 0;
-  font-size: 24px;
-}
-.btn {
-  background-color: #4CAF50;
-  color: white;
-  padding: 12px;
-  margin: 10px 0;
-  border: none;
-  width: 100%;
-  border-radius: 3px;
-  cursor: pointer;
-  font-size: 17px;
-}
-.btn {
-  background-color: #45a049;
-}
-a {
-  color: #2196F3;
-}
-hr {
-  border: 1px solid lightgrey;
-}
-
-table{
-  text-align: left;
-}
-th{
-  text-align:left;
-  float:left;
-  padding:10px;
-}
-tbody{
-  width: 95%;
-  
-}
-span.price {
-  float: right;
-  color: grey;
-}
-span.cart {
-  float: left;
-  color: grey;
-}
-@media (max-width: 800px) {
-  .row {
-    flex-direction: column-reverse;
-  }
-  .col-25 {
-    margin-bottom: 20px;
-  }
-}
-.Productname{
-  float:left;
-  padding: 2% 2%;
-}
-form {
-  float:left;
-  border:0%;
-  padding:0px;
-}
-.container form {
-  padding: 20px;
-}
-</style>
 
 <body>
   <div class="container">
--- products.php
+++ products.php
@@ -5,27 +5,28 @@ template_footer();
 ?>
 	<div class="products">
 	<h1>Products</h1>
-    <div class="container">
+	<ul>
     <?php 
         
     	$query_producttable = "SELECT name, description, price, inventory, idproduct, image FROM products";
     	$result_producttable = $conn->query($query_producttable);
     	
     	if ($result_producttable->num_rows > 0) {
+
     	    while($product = $result_producttable->fetch_assoc()) {
                 ?>
+                <li>
                 <a href="index.php?page=product&id=<?=$product['idproduct']?>">
-    	        <form method="post">
     	        	<img src="<?php echo $product["image"]?>"><br>
     	        	<?php echo $product["name"]?><br>
-    	        	<?php echo $product["price"]." :-"?><br>
-    	        </form>
+    	        	<?php echo $product["price"]." :-"?>
     	        </a>
+    	        </li>
   			<?php 
     	    }
     	    $conn->close();
     	}
         ?>
-        </div> 
+        </ul>
     </div> 
      
