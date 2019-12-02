45bc44b223fef0f01a8aff224ae9e4bb1074e3cd
wilmakrutrok <47818758+wilmakrutrok@users.noreply.github.com>
2019-12-02 18:19:15 +0100

    Add files via upload

--- admin.php
+++ admin.php
@@ -1,7 +1,6 @@
 <?php
   admin_verify();
   template_header('admin');
-
   if(isset($_POST['submit'])){
         $file = $_FILES['image'];
         $fileName = $_FILES['image']['name'];
@@ -11,25 +10,26 @@
         $fileType = $_FILES['image']['type'];
         $fileExt = explode('.', $fileName);
         $fileActualExt = strtolower(end($fileExt));
-
         if($fileError === 0){
           $fileNameNew = uniqid('',true).".".$fileActualExt;
           $fileDestination ='/'.$fileNameNew;
          move_uploaded_file($fileTmpName, $fileNameNew);
          header("Location: index.php?page=admin");
-
-
-
         }else{
           echo"There is something wrong with the file!";
         }
-
         $stmt =$conn->prepare("INSERT INTO products(price, inventory, name, description, image) VALUES(?, ?, ?, ?, ?)");
         $stmt->bind_param('iisss',$_POST['price'], $_POST['inventory'], $_POST['name'], $_POST['description'], $fileNameNew);
         $stmt->execute();
         header('Location: index.php?page=admin');
 }
-  ?>
+if(isset($_POST["edit"])){
+    $stmt = "UPDATE products
+            SET inventory = '".$_POST['newinventory']."'
+            WHERE idproduct = '".$_POST['idproduct']."'";
+    $result = mysqli_query($conn, $stmt);
+}
+?>
 <div class="container">
     <div class="div_cart_content">
       <div class="checkout_page">
@@ -63,6 +63,36 @@
      </form>
   </table>
 </div>
-  </div>
-    
+
+
+<div class="products" style="margin-bottom: 400px">
+<h1>Change inventory</h1>
+	<ul>
+    <?php 
+        
+    	$query_producttable = "SELECT name, description, price, inventory, idproduct, image FROM products";
+    	$result_producttable = $conn->query($query_producttable);
+    	
+    	if ($result_producttable->num_rows > 0) {
+
+    	    while($product = $result_producttable->fetch_assoc()) {
+                ?>
+                <li>
+                	<form method="post">
+        	        	<img src="<?php echo $product["image"]?>"><br>
+        	        	<?php echo $product["name"]?><br>
+        	        	<?php echo $product["price"]." :-"?><br>
+        	        	<input type="text" name="newinventory" placeholder="<?php echo $product["inventory"]." in stock"?>"><br>
+        	        	<input type="hidden" name="idproduct" value="<?php echo $product["idproduct"]?>"><br>
+        	        	<input type="submit" value="Edit" name="edit">
+    	        	</form>
+    	        </li>
+  			<?php 
+    	    }
+    	    $conn->close();
+    	}
+        ?>
+        </ul>
+    </div> 
+      </div>
 <?php template_footer();?>
