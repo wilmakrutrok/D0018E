51ad872c279a52fadfb45f0c95e263bbdd5a9f0b
wilmakrutrok <47818758+wilmakrutrok@users.noreply.github.com>
2019-12-13 11:36:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 session_start();
 include "config.php";
 
+//Check if user has logged in to give access to web shop else direct to login page
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
     $page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
 } else {
