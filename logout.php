8af8495fa332445b9c10db8088fe2aaf63f5920e
wilmakrutrok <47818758+wilmakrutrok@users.noreply.github.com>
2019-12-13 11:43:23 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-//End session for user and admin
+//Change session for user and admin so they can't reach pages anymore without loggin in
 $_SESSION['loggedin'] = false;
 $_SESSION['admin'] = false;
 header('Location: index.php');
